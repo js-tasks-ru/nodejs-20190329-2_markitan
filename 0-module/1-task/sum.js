21087c6d18923fffb3747417329861b67730a3f8
markitan <vasiliy.markitan@gmail.com>
2019-04-01 21:13:47 +0300

    update

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,10 +1,8 @@
 function sum(a, b) {
   if (typeof a === 'number' && 
     typeof b === 'number'
-  ) {
-    return a+b;
-  }
-  throw new TypeError('Arguments is not a number');
+  ) {return a+b;}
+  throw new TypeError('Arguments is not a numbers');
 }
 
 module.exports = sum;
