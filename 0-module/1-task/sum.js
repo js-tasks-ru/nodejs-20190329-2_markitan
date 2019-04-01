cd0bcadac872798fd4f9dc8053eec8fc9bb27630
markitan <vasiliy.markitan@gmail.com>
2019-04-01 21:16:22 +0300

    test update (second mac)

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,7 +1,9 @@
 function sum(a, b) {
   if (typeof a === 'number' && 
     typeof b === 'number'
-  ) {return a+b;}
+  ) {
+    return a+b;
+  }
   throw new TypeError('Arguments is not a numbers');
 }
 
